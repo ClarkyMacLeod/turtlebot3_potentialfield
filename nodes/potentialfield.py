64e05249df093a71716207d1d7af9bbcc8042166
ClarkyMacLeod <fleischy_3@web.de>
2020-04-02 09:14:18 +0200

    class Obstacle_Avoidance added

--- nodes/potentialfield.py
+++ nodes/potentialfield.py
@@ -14,89 +14,94 @@ max_vel = 0.26
 max_rot = 1.82
 pub = None
 
-def make_vector(msg):
-    global R
-    #distances above R are not important
-    ranges = [R if x > R else x for x in msg.ranges]
-    #distances below r are to be discarded
-    ranges = no_zeros(ranges)
-    #lower distance has higher effect
-    ranges = [1 - (x/R) for x in ranges]
-    return ranges
-
-def no_zeros(ranges):
-	global r
-	temp = []
-	
-	for x in ranges:
-		if (x>r):
-			temp.append(x)
-
-	return temp
-
-def callback(msg):
-    global A
-    global max_vel
-    global max_rot
-    global pub
-
-    ranges = make_vector(msg)
-
-    #make a vector of each entry in ranges
-    for i, e in enumerate(ranges):
-        ranges[i] = (e * math.cos(math.radians(i)) , e * math.sin(math.radians(i)))
+class Obstacle_Avoidance(self, Node):
     
-    #sum of all vectors
-    vector = (0,0)
-    for i, e in enumerate(ranges):
-        vector[0] += e[0]
-        vector[1] += e[1]
+    def __init__(self)
+        # publisher and subscriber
+        laser_sub = self.create_subscription("scan", LaserScan, callback)
+        pub = rclpy.Publisher("cmd_vel", Twist, queue_size = 20)
 
-    #flip direction and wrap in matrix
-    vector = np.matrix([[-vector[0]],[-vector[1]]])
+        # behavior on shutdown
+        rclpy.on_shutdown(stop)
 
-    #print vector
-    print("Vector = %f; %f" % (vector[0], vector[1]))
+    def make_vector(msg):
+        global R
+        #distances above R are not important
+        ranges = [R if x > R else x for x in msg.ranges]
+        #distances below r are to be discarded
+        ranges = no_zeros(ranges)
+        #lower distance has higher effect
+        ranges = [1 - (x/R) for x in ranges]
+        return ranges
 
-    #rotate vector by angle offset
-    rot = np.matrix([[math.cos(-msg.angle_min) , -math.sin(-msg.angle_min)], [math.sin(-msg.angle_min) , math.cos(-msg.angle_min)]])
-    vector = rot * vector
+    def no_zeros(ranges):
+        global r
+        temp = []
+        
+        for x in ranges:
+            if (x>r):
+                temp.append(x)
 
-    #add constant attraction from front
-    vector[0] += A
+        return temp
 
-    #norm vector to not exceed max velocity
-    vector = vector/np.linalg.norm(vector)
+    def callback(msg):
+        global A
+        global max_vel
+        global max_rot
+        global pub
 
-    #make direction message
-    dir = Twist()
-    dir.linear.x = max_vel * vector[0]
-    dir.angular.z = max_rot * math.asin(vector[1]/math.hypot(vector[0],vector[1]))
+        ranges = make_vector(msg)
 
-    #print direction
-    print("Dir = %f; %f" % (dir.linear.x, dir.angular.z))
-    
-    pub.publish(dir)
+        #make a vector of each entry in ranges
+        for i, e in enumerate(ranges):
+            ranges[i] = (e * math.cos(math.radians(i)) , e * math.sin(math.radians(i)))
+        
+        #sum of all vectors
+        vector = (0,0)
+        for i, e in enumerate(ranges):
+            vector[0] += e[0]
+            vector[1] += e[1]
+
+        #flip direction and wrap in matrix
+        vector = np.matrix([[-vector[0]],[-vector[1]]])
+
+        #print vector
+        print("Vector = %f; %f" % (vector[0], vector[1]))
+
+        #rotate vector by angle offset
+        rot = np.matrix([[math.cos(-msg.angle_min) , -math.sin(-msg.angle_min)], [math.sin(-msg.angle_min) , math.cos(-msg.angle_min)]])
+        vector = rot * vector
+
+        #add constant attraction from front
+        vector[0] += A
+
+        #norm vector to not exceed max velocity
+        vector = vector/np.linalg.norm(vector)
+
+        #make direction message
+        dir = Twist()
+        dir.linear.x = max_vel * vector[0]
+        dir.angular.z = max_rot * math.asin(vector[1]/math.hypot(vector[0],vector[1]))
+
+        #print direction
+        print("Dir = %f; %f" % (dir.linear.x, dir.angular.z))
+        
+        pub.publish(dir)
 
 
-    
-def stop():
-    # stop robot when node is stopped
-    pub.publish(Twist())
+        
+    def stop():
+        # stop robot when node is stopped
+        pub.publish(Twist())
 
 
 def potentialfield():
     global pub 
 
     # initialize node
-    rclpy.init_node('obstacle_avoidance', anonymous=True)
-
-    # publisher and subscriber
-    rclpy.Subscriber("scan", LaserScan, callback)
-    pub = rclpy.Publisher("cmd_vel", Twist, queue_size = 20)
-
-    # behavior on shutdown
-    rclpy.on_shutdown(stop)
+    rclpy.init(args=args)
+    potentialfield = Obstacle_Avoidance()
+    
     # infinite loop
     rclpy.spin()
 
